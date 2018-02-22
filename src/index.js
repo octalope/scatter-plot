579a688e8994c2f0935b76e7395200ad3f3f4f09
Steve Brooks <steve.c.brooks@gmail.com>
2018-02-21 23:08:24 -0500

    axes, labels, data...

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const fs       = require('fs');
 const jsdom    = require('jsdom');
 const chart    = require('../lib/chart');
 
-var indices    = Array.apply(null, {length: 10}).map(Number.call, Number);
+var indices    = Array.apply(null, {length: 50}).map(Number.call, Number);
 var data = indices.map(i => {
   var f = x => Math.sin(x);
-  var x = (i/(indices.length-1)) * Math.PI;
+  var x = (i/(indices.length-1)) * 3 * Math.PI;
   return { x: x, y: f(x) };
 });
 
